a47d82560dc946d5e7dffa033055d90f2133c5f1
skolach <ssskolach@gmail.com>
2021-11-23 17:27:41 +0200

    added @PostConstruvt, @PreDestroy in BeanE

--- hw_3/src/main/java/com/epam/spring/hw_3/beans/BeanE.java
+++ hw_3/src/main/java/com/epam/spring/hw_3/beans/BeanE.java
@@ -1,5 +1,8 @@
 package com.epam.spring.hw_3.beans;
 
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+
 import org.springframework.stereotype.Component;
 
 @Component
@@ -7,6 +10,16 @@ public class BeanE {
     
     private String name;
     private String value;
+
+    @PostConstruct
+    public void postConstructOfBeanF(){
+        System.out.println("in postConstructOfBeanF() annotated as @PostConstruct");
+    }
+
+    @PreDestroy
+    public void preDestroyOfBeanF(){
+        System.out.println("in preDestroyOfBeanF() annotated as @PreDestroy");
+    }
 
     public String getName() {
         return name;
