1ce79253f8438dc91a044fd7a3ee8ec7f6e34e4e
skolach <ssskolach@gmail.com>
2021-11-23 17:28:53 +0200

    removed unnecessary annotation

--- hw_3/src/main/java/com/epam/spring/hw_3/config/OtherConfig.java
+++ hw_3/src/main/java/com/epam/spring/hw_3/config/OtherConfig.java
@@ -1,10 +1,8 @@
 package com.epam.spring.hw_3.config;
 
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-@ComponentScan("com.epam.spring.hw_3.other")
 public class OtherConfig {
-    
+
 }
