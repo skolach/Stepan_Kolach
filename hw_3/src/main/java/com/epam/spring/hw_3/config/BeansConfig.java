4d70717d78de73634f50a2f3f9ff9d8b3d483ed7
skolach <ssskolach@gmail.com>
2021-11-24 01:08:20 +0200

    refactoring

--- hw_3/src/main/java/com/epam/spring/hw_3/config/BeansConfig.java
+++ hw_3/src/main/java/com/epam/spring/hw_3/config/BeansConfig.java
@@ -5,6 +5,7 @@ import com.epam.spring.hw_3.beans.BeanC;
 import com.epam.spring.hw_3.beans.BeanD;
 import com.epam.spring.hw_3.beans.BeanFactoryPostProcessorBean;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
@@ -18,18 +19,18 @@ import org.springframework.context.annotation.PropertySource;
 public class BeansConfig {
 
     @Bean(name = "beanB", initMethod = "init", destroyMethod = "destroy")
-    public BeanB getBeanB(){
-        return new BeanB();
+    public BeanB getBeanB(@Value("${BeanB.name}") String name, @Value("${BeanB.value}") Long value){
+        return new BeanB(name, value);
     }
 
     @Bean(name = "beanC", initMethod = "init", destroyMethod = "destroy")
-    public BeanC getBeanC(){
-        return new BeanC();
+    public BeanC getBeanC(@Value("${BeanC.name}") String name, @Value("${BeanC.value}") Long value){
+        return new BeanC(name, value);
     }
 
     @Bean(name = "beanD", initMethod = "init", destroyMethod = "destroy")
-    public BeanD getBeanD(){
-        return new BeanD();
+    public BeanD getBeanD(@Value("${BeanD.name}") String name, @Value("${BeanD.value}") Long value){
+        return new BeanD(name, value);
     }
 
     @Bean
